acfb08f9f51c1ce52642f77e61f186315b4c6269
Administrator <Administrator@USER-20161024GP>
2017-09-07 12:46:12 +0800

    上传朱子ribbon断路器

--- springcloudribbon/src/main/java/com/xxf/springcloud/controller/ConsumerController.java
+++ springcloudribbon/src/main/java/com/xxf/springcloud/controller/ConsumerController.java
@@ -4,14 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+
+import com.xxf.springcloud.service.ComputeService;
 
 @RestController
 public class ConsumerController {
     @Autowired
-    RestTemplate restTemplate;
+    private ComputeService computeService;
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public String add() {
-        return restTemplate.getForEntity("http://COMPUTE-SERVICE/add?a=10&b=20", String.class).getBody();
+    	return computeService.addService();
+        //return restTemplate.getForEntity("http://COMPUTE-SERVICE/add?a=10&b=20", String.class).getBody();
     }
 }

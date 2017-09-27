c4d6acdc0cac109f16388e9a5d369160b56259b2
Administrator <Administrator@USER-20161024GP>
2017-09-27 14:07:25 +0800

    1

--- springcloudzuul/src/main/java/com/xxf/springcloud/zuul/Application.java
+++ springcloudzuul/src/main/java/com/xxf/springcloud/zuul/Application.java
@@ -13,8 +13,10 @@ public class Application {
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(Application.class).web(true).run(args);
 	}
+    
 	@Bean
 	public AccessFilter accessFilter() {
 		return new AccessFilter();
 	}
+	
 }
